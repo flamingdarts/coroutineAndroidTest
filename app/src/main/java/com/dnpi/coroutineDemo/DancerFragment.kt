e8e3d9f3246dfd921671dfc32623b961c3cbc37d
Phung, Danh-Nhan (Extern) <danh-nhan.phung.extern@jsmd-group.com>
2020-03-11 14:34:11 +0100

    clean code

--- app/src/main/java/com/dnpi/coroutineDemo/DancerFragment.kt
+++ app/src/main/java/com/dnpi/coroutineDemo/DancerFragment.kt
@@ -26,10 +26,7 @@ class DancerFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dancer, container, false)
-    }
+    ): View? = inflater.inflate(R.layout.fragment_dancer, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
